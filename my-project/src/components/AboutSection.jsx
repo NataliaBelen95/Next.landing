268462204522be1db9cf235dc7f1595a6c5aef08
Damian Figueroa <damy.mtl@gmail.com>
2023-05-10 14:36:41 -0300

    trabajando

--- my-project/src/components/AboutSection.jsx
+++ my-project/src/components/AboutSection.jsx
@@ -4,6 +4,7 @@ import style from "../styles/AboutSection.module.css";
 
 export default function AboutSection() {
   return (
+<<<<<<< Updated upstream
     <section class="bg-white ">
       <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
         <div class=" text-black sm:text-lg ">
@@ -36,5 +37,26 @@ export default function AboutSection() {
         </div>
       </div>
     </section>
+=======
+    <section class="bg-white" >
+  <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+    <div class="text-black sm:text-lg">
+      <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">
+        Sobre Nosotros
+      </h2>
+      <p class="mb-4">
+        Fleitas S.R.L es una empresa de construcción con más de 15 años de experiencia en el diseño, construcción y mantenimiento de hogares, edificios y obras civiles para clientes privados y públicos en todo el país. 
+      </p>
+      <p>
+        Nos especializamos en soluciones personalizadas y eficientes para cada proyecto, trabajando en estrecha colaboración con nuestros clientes para entender sus necesidades y brindarles el mejor servicio.
+      </p>
+    </div>
+    <div class="grid grid-cols-2 gap-4 mt-8">
+      <img class="w-full rounded-lg shadow-xl" src="/obra1/exterior1.jpg" alt="office content 1"/>
+      <img class="mt-4 w-full lg:mt-10 rounded-lg shadow-xl" src="/obra1/exterior2.jpg" alt="office content 2"/>
+    </div>
+  </div>
+</section>
+>>>>>>> Stashed changes
   );
 }
--- my-project/src/components/Services.jsx
+++ my-project/src/components/Services.jsx
@@ -1,9 +1,12 @@
+import Image from 'next/image'
+import { motion } from "framer-motion";
+import { useEffect, useRef } from "react";
 import image1 from '../assets/constructora/servicios/1.png'
 import image2 from '../assets/constructora/servicios/2.png'
 import image3 from '../assets/constructora/servicios/3.png'
 import image4 from '../assets/constructora/servicios/4.png'
-import Image from 'next/image'
 
+<<<<<<< Updated upstream
 export default function Services(){
     const services = [ 
         { 
@@ -22,34 +25,77 @@ export default function Services(){
             titulo: "Pisos, Piletas industriales y Demoliciones",
             description: "superficies de concreto para soportar cargas",
             image: image4
+=======
+export default function Services() {
+  const services = [
+    {
+      titulo: "Obras Civiles",
+      description: "viviendas, edificios publicos y privados",
+      image: image1
+    },
+    {
+      titulo: "Servicios Industriales",
+      description: "cloacas, pluviales, instalaciones de gas, instalaciones electricas y tendido de cañerias",
+      image: image2
+    },
+    {
+      titulo: "Obras Viales",
+      description: "movimiento de suelo, pavimentos de hormigon",
+      image: image3
+    },
+    {
+      titulo: "Pisos, Piletas industriales y Demoliciones",
+      description: "superficies de concreto para soportar cargas",
+      image: image4
+    }
+  ];
+
+  const observer = useRef(null);
+
+  useEffect(() => {
+    observer.current = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("in-view");
+        } else {
+          entry.target.classList.remove("in-view");
+>>>>>>> Stashed changes
         }
-    ]
+      });
+    });
 
+    document.querySelectorAll(".card").forEach(card => {
+      observer.current.observe(card);
+    });
 
-    
-    return (
-        <section class="max-w-7xl mx-auto py-5 flex flex-col justify-center items-center">
-  <h1 class="text-center text-3xl font-bold mb-8">Presupuesto, construcción y mantenimiento de:</h1>
-  <div class="grid grid-cols-1 sm:grid-cols-2 gap-12">
-    {services.map((service, index) => (
-      <div class="bg-gray-100 shadow-xl rounded-2xl p-2 m-2 
-                   md:max-w-[350px] md:h-130 
-                   sm:w-150 sm:h-300 
-                   lg:w-150 lg:h-250 
-                   flex flex-col justify-center items-center 
-                   sm:my-4">
-        <div class="w-full flex justify-center">
-          <Image width={130} height={130} src={service.image} alt="service" />
-        </div>
-        <div class="mt-4 text-center">
-          <h3 class="font-extrabold mb-2">{service.titulo}</h3>
-          <p class="text-sm">{service.description}</p>
-        </div>
+    return () => {
+      observer.current.disconnect();
+    };
+  }, []);
+
+  return (
+    <section className="max-w-7xl mx-auto py-5 flex flex-col justify-center items-center">
+      <h1 className="text-center text-3xl font-bold mb-8">Presupuesto, construcción y mantenimiento de:</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-12">
+        {services.map((service, index) => (
+          <motion.div
+            key={index}
+            className="bg-gray-100 shadow-xl rounded-2xl p-2 m-2 md:max-w-[350px] md:h-130 sm:w-150 sm:h-300 lg:w-150 lg:h-250 flex flex-col justify-center items-center sm:my-4 card"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: index * 0.1 }}
+            whileHover={{ scale: 1.05 }}
+          >
+            <div className="w-full flex justify-center">
+              <Image width={130} height={130} src={service.image} alt="service" />
+            </div>
+            <div className="mt-4 text-center">
+              <h3 className="font-extrabold mb-2">{service.titulo}</h3>
+              <p className="text-sm">{service.description}</p>
+            </div>
+          </motion.div>
+        ))}
       </div>
-    ))}
-  </div>
-</section>
-      
-
-    )
+    </section>
+  );
 }
