8a570aa07e7f6360af03a00fbc58cb62d42b76dd
Damian Figueroa <damy.mtl@gmail.com>
2023-05-10 16:00:01 -0300

    efectos de trnasicion

--- my-project/src/components/AboutSection.jsx
+++ my-project/src/components/AboutSection.jsx
@@ -1,62 +1,55 @@
-import magicImage from "../assets/constructora/magic.jpg";
-import Image from "next/image";
-import style from "../styles/AboutSection.module.css";
+import { useSpring, animated } from "react-spring";
 
 export default function AboutSection() {
+  const textProps = useSpring({
+    from: { opacity: 0, y: 50 },
+    to: { opacity: 1, y: 0 },
+    delay: 500
+  });
+
+  const imageProps = useSpring({
+    from: { opacity: 0, x: 50 },
+    to: { opacity: 1, x: 0 },
+    delay: 1000
+  });
+
   return (
-<<<<<<< Updated upstream
-    <section class="bg-white ">
-      <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
-        <div class=" text-black sm:text-lg ">
-          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">
+    <section className="bg-white">
+      <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+        <animated.div style={textProps} className="text-black sm:text-lg">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">
             Sobre Nosotros
           </h2>
-          <p class="mb-4">
-            {" "}
-            Nos especializamos en la construcción, mantenimiento y presupuesto
-            de obras civiles, incluyendo viviendas y edificios públicos y
-            privados.
+          <p className="mb-4">
+            Fleitas S.R.L es una empresa de construcción con más de 15 años de
+            experiencia en el diseño, construcción y mantenimiento de hogares,
+            edificios y obras civiles para clientes privados y públicos en todo
+            el país.
           </p>
           <p>
-            En Fleitas, nos comprometemos a trabajar en estrecha colaboración
-            con nuestros clientes para entender sus necesidades y ofrecer
-            soluciones personalizadas y eficientes para cada proyecto.
+            Nos especializamos en soluciones personalizadas y eficientes para
+            cada proyecto, trabajando en estrecha colaboración con nuestros
+            clientes para entender sus necesidades y brindarles el mejor
+            servicio.
           </p>
-        </div>
-        <div class="grid grid-cols-2 gap-4 mt-8">
-          <img
-            class="w-full rounded-lg"
+        </animated.div>
+        <div className="grid grid-cols-2 gap-4 mt-8">
+          <animated.img
+            style={imageProps}
+            className="w-full rounded-lg shadow-xl"
             src="/obra1/exterior1.jpg"
             alt="office content 1"
           />
-          <img
-            class="mt-4 w-full lg:mt-10 rounded-lg"
+          <animated.img
+            style={imageProps}
+            className="mt-4 w-full lg:mt-10 rounded-lg shadow-xl"
             src="/obra1/exterior2.jpg"
             alt="office content 2"
           />
         </div>
       </div>
     </section>
-=======
-    <section class="bg-white" >
-  <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
-    <div class="text-black sm:text-lg">
-      <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">
-        Sobre Nosotros
-      </h2>
-      <p class="mb-4">
-        Fleitas S.R.L es una empresa de construcción con más de 15 años de experiencia en el diseño, construcción y mantenimiento de hogares, edificios y obras civiles para clientes privados y públicos en todo el país. 
-      </p>
-      <p>
-        Nos especializamos en soluciones personalizadas y eficientes para cada proyecto, trabajando en estrecha colaboración con nuestros clientes para entender sus necesidades y brindarles el mejor servicio.
-      </p>
-    </div>
-    <div class="grid grid-cols-2 gap-4 mt-8">
-      <img class="w-full rounded-lg shadow-xl" src="/obra1/exterior1.jpg" alt="office content 1"/>
-      <img class="mt-4 w-full lg:mt-10 rounded-lg shadow-xl" src="/obra1/exterior2.jpg" alt="office content 2"/>
-    </div>
-  </div>
-</section>
->>>>>>> Stashed changes
   );
 }
+
+
--- my-project/src/components/Services.jsx
+++ my-project/src/components/Services.jsx
@@ -1,99 +1,55 @@
 import Image from 'next/image'
-import { motion } from "framer-motion";
+import { useSpring, animated } from 'react-spring'
 import { useEffect, useRef } from "react";
 import image1 from '../assets/constructora/servicios/1.png'
 import image2 from '../assets/constructora/servicios/2.png'
 import image3 from '../assets/constructora/servicios/3.png'
 import image4 from '../assets/constructora/servicios/4.png'
+import { useInView } from 'react-intersection-observer';
 
-<<<<<<< Updated upstream
-export default function Services(){
-    const services = [ 
-        { 
-            titulo: "Obras Civiles",
-            description: "viviendas, edificios publicos y privados",
-            image: image1
-        },{
-            titulo: "Obras de Infraestructura",
-            description: "cloacas, pluviales, instalaciones de gas, instalaciones electricas y tendido de cañerias",
-            image: image2
-        },{
-            titulo: "Obras Viales",
-            description: "movimiento de suelo, pavimentos de hormigon",
-            image: image3
-        },{
-            titulo: "Pisos, Piletas industriales y Demoliciones",
-            description: "superficies de concreto para soportar cargas",
-            image: image4
-=======
+
 export default function Services() {
   const services = [
     {
-      titulo: "Obras Civiles",
+      title: "Obras Civiles",
       description: "viviendas, edificios publicos y privados",
       image: image1
     },
     {
-      titulo: "Servicios Industriales",
+      title: "Servicios Industriales",
       description: "cloacas, pluviales, instalaciones de gas, instalaciones electricas y tendido de cañerias",
       image: image2
     },
     {
-      titulo: "Obras Viales",
+      title: "Obras Viales",
       description: "movimiento de suelo, pavimentos de hormigon",
       image: image3
     },
     {
-      titulo: "Pisos, Piletas industriales y Demoliciones",
+      title: "Pisos, Piletas industriales y Demoliciones",
       description: "superficies de concreto para soportar cargas",
       image: image4
     }
   ];
 
-  const observer = useRef(null);
-
-  useEffect(() => {
-    observer.current = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("in-view");
-        } else {
-          entry.target.classList.remove("in-view");
->>>>>>> Stashed changes
-        }
-      });
-    });
-
-    document.querySelectorAll(".card").forEach(card => {
-      observer.current.observe(card);
-    });
-
-    return () => {
-      observer.current.disconnect();
-    };
-  }, []);
+  const [ref, inView] = useInView({ threshold: 0.1 });
+  const props = useSpring({ opacity: inView ? 1 : 0, y: inView ? 0 : 50 });
 
   return (
     <section className="max-w-7xl mx-auto py-5 flex flex-col justify-center items-center">
       <h1 className="text-center text-3xl font-bold mb-8">Presupuesto, construcción y mantenimiento de:</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-12">
         {services.map((service, index) => (
-          <motion.div
+          <animated.div
             key={index}
-            className="bg-gray-100 shadow-xl rounded-2xl p-2 m-2 md:max-w-[350px] md:h-130 sm:w-150 sm:h-300 lg:w-150 lg:h-250 flex flex-col justify-center items-center sm:my-4 card"
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: index * 0.1 }}
-            whileHover={{ scale: 1.05 }}
+            ref={ref}
+            className={`bg-gray-100 shadow-xl rounded-2xl p-2 m-2 md:max-w-[350px] md:h-130 sm:w-150 sm:h-300 lg:w-150 lg:h-250 flex flex-col justify-center items-center sm:my-4 card`}
+            style={props}
           >
-            <div className="w-full flex justify-center">
-              <Image width={130} height={130} src={service.image} alt="service" />
-            </div>
-            <div className="mt-4 text-center">
-              <h3 className="font-extrabold mb-2">{service.titulo}</h3>
-              <p className="text-sm">{service.description}</p>
-            </div>
-          </motion.div>
+            <Image width={130} height={130} src={service.image} alt="service" />
+            <h3 className="text-lg mt-2 font-semibold">{service.title}</h3>
+            <p className="text-center text-gray-600">{service.description}</p>
+          </animated.div>
         ))}
       </div>
     </section>
